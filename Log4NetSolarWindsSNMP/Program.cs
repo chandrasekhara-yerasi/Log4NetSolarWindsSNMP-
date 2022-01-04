ae0b3ec47cb5a2b56b9ab633f554b33ed82c662c
Chandrasekhara Yerasi <chandrasekhara.yerasi@n-able.com>
2022-01-03 20:55:01 -0600

    test strong name assembly resolving when they have different public key tokens

--- Log4NetSolarWindsSNMP/Program.cs
+++ Log4NetSolarWindsSNMP/Program.cs
@@ -11,8 +11,11 @@ namespace Log4NetSolarWindsSNMP
             currentDomain.AssemblyResolve += new ResolveEventHandler(MyResolveEventHandler);
             currentDomain.AssemblyLoad += new AssemblyLoadEventHandler(MyLoadEventHandler);
 
-                Console.WriteLine("Solarwinds SNMP start");
-                SNMPPriv snmpPriv = SNMPPriv.None;
+            testResolveForStrongNameAssembliesWithDifferentPublicTokens();
+
+            Console.WriteLine("Solarwinds SNMP start");
+
+            SNMPPriv snmpPriv = SNMPPriv.None;
                 SNMPAuth snmpAuth = SNMPAuth.None;
                 SNMPRequest request = new SNMPRequest();
                 request.SessionHandle.AuthType = snmpAuth;
@@ -46,5 +49,36 @@ namespace Log4NetSolarWindsSNMP
 
             }
         }
+
+        private static void testResolveForStrongNameAssembliesWithDifferentPublicTokens()
+        {
+            String type = "log4net.Core.LogException";
+            String assemblyWithDifferentPublicToken = "log4net, Version=1.2.10.0, Culture=neutral, PublicKeyToken=1b44e1d426115821";
+            String assemblyWithSamePublicToken = "log4net, Version=1.2.10.0, Culture=neutral, PublicKeyToken=669e0ddf0bb1aa2a";
+
+            Console.WriteLine("Solarwinds strong name assembly test start");
+
+            LoadStrongNameAssemblyAndCreateTypeInstance(assemblyWithDifferentPublicToken, type); // PublicKeyToken different from log4net 2.x dll version and hence cannot be resolved
+            LoadStrongNameAssemblyAndCreateTypeInstance(assemblyWithSamePublicToken, type); // PublicKeyToken same as log4net 2.x dll version and hence can be resolved
+
+            Console.WriteLine("Solarwinds strong assembly test end");
+
+        }
+        private static void LoadStrongNameAssemblyAndCreateTypeInstance(string assemblyName, String type)
+        {
+            AppDomain ad = AppDomain.CurrentDomain;
+
+            try
+            {
+                object obj = ad.CreateInstanceAndUnwrap(assemblyName, type);
+                Console.WriteLine("Resolved {0} from {1}", type, assemblyName);
+                            
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception during Load " + ex.Message);
+            }
+        }
+
     }
 }
